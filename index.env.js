c87a0410df822631d568258d400f909b09c78c42
Nuogz <zheung@vip.qq.com>
2023-06-02 14:42:12 +0800

    improve fetch-log; import `meta` for easy use; parse and save raw gacha log

--- index.env.js
+++ index.env.js
@@ -3,7 +3,7 @@ process.env.NENV_PKG_PATH = import.meta.url;
 process.env.NENV_I18N_LOCALE = 'zh';
 process.env.NENV_I18N_FORMAT = 'hades';
 
-process.env.NENV_PANGU = 'command;config;day';
+process.env.NENV_PANGU = 'command;config;day;poseidon';
 
 
 /** @param {import('commander').Command} CMD */
